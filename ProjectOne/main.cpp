7437906c8a93e36ba8b4a815af596d572d90c2d6
niniko1997 <m.nino97@gmail.com>
2017-04-16 18:34:03 -0700

    added project description

--- ProjectOne/main.cpp
+++ ProjectOne/main.cpp
@@ -1,1 +1,13 @@
-"//Project 1" 
+//Project 1
+
+//The goal of this Project is to program the Spanish game "Siete y Medio"
+	// The goal of the total game is to get as close as possible to 7.5 without going over it
+
+
+// initial starting values for the game
+int PLAYER_MONEY = 100; 
+int MAX_DEALER_LOSS = 900;
+
+int main(){
+
+}// end main
