416e317617eadcc8c5cb8f809ea978bed5202c6f
niniko1997 <m.nino97@gmail.com>
2017-05-24 21:48:28 -0700

    added schemas for additional classes

--- ProjectTwo/grade_calculator.cpp
+++ ProjectTwo/grade_calculator.cpp
@@ -40,6 +40,8 @@ void grade_calculator::on_radioButton5_Clicked(){
 }
 
 // calculates average hw grade, average midterm grade, final grade and final project grade for the PIC10C class
+// It takes into account two different grading schemes (15% hw + 25% * midterm + 30% final exam + 30% final project) OR
+// (15% hw + 50% final exam + 35% final project) and return the better grade of the two
 double grade_calculator::calculatePIC10C(){
     double averageHWGrade = 0;
     double averageMidtermGrade = 0;
@@ -70,6 +72,69 @@ double grade_calculator::calculatePIC10C(){
     return overallGrade2;
 }
 
+// calculates average hw grade, average midterm grade and final exam grade Math 132 class
+// it only uses one schema (10% hw + 10% quiz + 50% final exam + 30% best midterm score)
+double grade_calculator::calculateMath132(){
+    double averageHWGrade = 0;
+    double bestMidtermGrade = 0;
+    double finalGrade = 0;
+    double quizGrade = 0;
+
+    // calculate average hw
+    double hwScore = ((ui->hwOneScore->text()).toDouble() + (ui->hwOneScore->text()).toDouble() + (ui->hwOneScore->text()).toDouble() + (ui->hwFourScore->text()).toDouble() + (ui->hwFiveScore->text()).toDouble() + (ui->hwSixScore->text()).toDouble() + (ui->hwSevenScore->text()).toDouble() + (ui->hwEightScore->text()).toDouble() + (ui->hwNineScore->text()).toDouble() + (ui->hwTenScore->text()).toDouble());
+    double hwTotal = ((ui->hwOneScoreTotal->text()).toDouble() + (ui->hwTwoScoreTotal->text()).toDouble() + (ui->hwThreeScoreTotal->text()).toDouble() + (ui->hwFourScoreTotal->text()).toDouble() + (ui->hwFiveScoreTotal->text()).toDouble() + (ui->hwSixScoreTotal->text()).toDouble() + (ui->hwSevenScoreTotal->text()).toDouble() + (ui->hwEightScoreTotal->text()).toDouble() + (ui->hwNineScoreTotal->text()).toDouble() + (ui->hwTenScoreTotal->text()).toDouble());
+    averageHWGrade = hwScore/hwTotal;
+
+    // calculate average midterm
+    double midtermOne = ((ui->midtermOneScore->text()).toDouble())/((ui->midtermOneScoreTotal->text()).toDouble());
+    double midtermTwo = ((ui->midtermTwoScore->text()).toDouble())/((ui->midtermTwoScoreTotal->text()).toDouble());
+
+    if(midtermOne >= midtermTwo){
+        bestMidtermGrade = midtermOne;
+    } else {
+        bestMidtermGrade = midtermTwo;
+    }
+
+    // calculate final exam grade
+    finalGrade = ((ui->finalScore->text()).toDouble())/((ui->finalScoreTotal->text()).toDouble());
+
+    // calculate quiz grade
+
+    double overallGrade = .10 * averageHWGrade + .1 * quizGrade + .50 * finalGrade + .30 * bestMidtermGrade;
+
+    return overallGrade;
+}
+
+// calculates average hw grade, average midterm grade and final exam grade Math 132 class
+// it only uses one schema (15% hw + 50% final exam + 35% best midterm score)
+double grade_calculator::calculateMath180(){
+    double averageHWGrade = 0;
+    double bestMidtermGrade = 0;
+    double finalGrade = 0;
+
+    // calculate average hw
+    double hwScore = ((ui->hwOneScore->text()).toDouble() + (ui->hwOneScore->text()).toDouble() + (ui->hwOneScore->text()).toDouble() + (ui->hwFourScore->text()).toDouble() + (ui->hwFiveScore->text()).toDouble() + (ui->hwSixScore->text()).toDouble() + (ui->hwSevenScore->text()).toDouble());
+    double hwTotal = ((ui->hwOneScoreTotal->text()).toDouble() + (ui->hwTwoScoreTotal->text()).toDouble() + (ui->hwThreeScoreTotal->text()).toDouble() + (ui->hwFourScoreTotal->text()).toDouble() + (ui->hwFiveScoreTotal->text()).toDouble() + (ui->hwSixScoreTotal->text()).toDouble() + (ui->hwSevenScoreTotal->text()).toDouble());
+    averageHWGrade = hwScore/hwTotal;
+
+    // calculate average midterm
+    double midtermOne = ((ui->midtermOneScore->text()).toDouble())/((ui->midtermOneScoreTotal->text()).toDouble());
+    double midtermTwo = ((ui->midtermTwoScore->text()).toDouble())/((ui->midtermTwoScoreTotal->text()).toDouble());
+
+    if(midtermOne >= midtermTwo){
+        bestMidtermGrade = midtermOne;
+    } else {
+        bestMidtermGrade = midtermTwo;
+    }
+
+    // calculate final exam grade
+    finalGrade = ((ui->finalScore->text()).toDouble())/((ui->finalScoreTotal->text()).toDouble());
+
+    double overallGrade = .15 * averageHWGrade + .50 * finalGrade + .35 * bestMidtermGrade;
+
+    return overallGrade;
+}
+
 // checks which scheme was chosen and calls the appropriate calculate function
 // outputs total score into the line edit field
 void grade_calculator::on_submit_Clicked(){
@@ -88,5 +153,3 @@ void grade_calculator::on_submit_Clicked(){
 
     ui->lineEdit->setText(QString::number(grade) + "%");
 }
-
-
