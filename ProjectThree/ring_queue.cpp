36d6c2d83594f29fadc3a263336c801ba360945d
niniko1997 <m.nino97@gmail.com>
2017-05-29 21:06:05 -0700

    fixed end() function

--- ProjectThree/ring_queue.cpp
+++ ProjectThree/ring_queue.cpp
@@ -155,14 +155,12 @@ class RingQueue{
         }
 
         iterator end() {
-            return iterator(this, ring_size -1);
+            return iterator(this, ring_size);
         }
 
         // Miscellaneous functions
         size_t size() const {
-            for(auto it = this->begin(); it->begin() != it->end(); it++){
-
-            }
+            return ring_size;
         }
 
         // Debugging functions
@@ -200,20 +198,12 @@ int main(){
     std::cout << '\n';
 
     
-
-    // Uncomment the block below only when you have a working 
-    // implementation of RingQueue<ItemType,int>::end(). 
-    // If the implementation is not correct, it might result in 
-    // an infinite loop.
-    /** 
     std::cout << "Queue via iterators: \n";
     for ( auto it = rq.begin() ; it != rq.end() ; ++it ) {
         std::cout << "Value: " << *it << ", address: " << &(*it) << '\n';
     }
     std::cout << '\n';
-    */
-
-
+    
 
     rq.dump_queue();
 
